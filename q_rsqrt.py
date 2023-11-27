f6ee78d1b901663d8d95ee678179d8ecddfd2be6
Inc0deus <131204528+Inc0deus@users.noreply.github.com>
2023-11-27 12:41:20 +0100

    Update q_rsqrt.py

--- q_rsqrt.py
+++ q_rsqrt.py
@@ -18,7 +18,7 @@ def q_rsqrt(inp):
 
     x2 = inp * 0.5
     y = inp
-    i = f_bin(y)                # evil floating point bit level hacking
+    i = f_bin(y)                # (the equivalent of) evil floating point bit level hacking
     i = 0x5f3759df - (i >> 1)   # what the fuck?
     y = bin_f(i)
     y = y * (threehalfs - (x2 * y * y))     # 1st iteration
