cc61e632b37cceeb8aa88f41ab6f1131f7a536f2
Inc0deus <131204528+Inc0deus@users.noreply.github.com>
2023-11-27 12:43:03 +0100

    Update q_rsqrt.py

--- q_rsqrt.py
+++ q_rsqrt.py
@@ -1,23 +1,19 @@
 import struct
-from math import *
-from time import *
-from random import *
 
+def f_bin(x):    # -> * ( long * ) &x
+    return int(format(struct.unpack('i', struct.pack('f', x))[0], '032b'), 2)
 
-def f_bin(num):
-    return int(format(struct.unpack('i', struct.pack('f', num))[0], '032b'), 2)
+def bin_f(x):    # -> * ( float * ) &x
+    return struct.unpack('f',struct.pack('i', x))[0]
 
-def bin_f(inp):
-    return struct.unpack('f',struct.pack('i', inp))[0]
-
-def q_rsqrt(inp):
+def q_rsqrt(number):
     i: int
     x2: float
     y: float
     threehalfs = 1.5
 
-    x2 = inp * 0.5
-    y = inp
+    x2 = number * 0.5
+    y = number
     i = f_bin(y)                # (the equivalent of) evil floating point bit level hacking
     i = 0x5f3759df - (i >> 1)   # what the fuck?
     y = bin_f(i)
@@ -25,21 +21,3 @@ def q_rsqrt(inp):
     # y = y * (threehalfs - (x2 * y * y))   # 2nd iteration, this can be removed
     
     return y
-
-
-N = 1000000
-l = [uniform(0.0001, 10000) for i in range(N)]
-
-t = time()
-for n in l:
-    k = q_rsqrt(n)
-t1 = time() - t
-print(t1)
-
-t = time()
-for n in l:
-    k = 1/sqrt(n)
-t2 = time() - t
-print(t2)
-
-print(t1/t2)
